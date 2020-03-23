01c51fa6b5ee61cf6bf36a69882f5655e2d5d88e
Krzysztof Urban <k.urban@g.pl>
2020-03-23 20:32:38 +0100

    Add kafka implementation

--- src/main/java/pl/krzysztofurban/springmessagingexample/kafka/KafkaConsumer.java
+++ src/main/java/pl/krzysztofurban/springmessagingexample/kafka/KafkaConsumer.java
@@ -1,4 +1,16 @@
 package pl.krzysztofurban.springmessagingexample.kafka;
 
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Service;
+
+import java.io.IOException;
+
+@Service
+@Slf4j
 public class KafkaConsumer {
+    @KafkaListener(topics = "orders", groupId = "order_grp")
+    public void consume(String message) throws IOException {
+        log.info("Kafka Received :: {}", message);
+    }
 }
--- src/main/java/pl/krzysztofurban/springmessagingexample/kafka/KafkaProducer.java
+++ src/main/java/pl/krzysztofurban/springmessagingexample/kafka/KafkaProducer.java
@@ -1,4 +1,25 @@
 package pl.krzysztofurban.springmessagingexample.kafka;
 
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Service;
+
+import java.util.Calendar;
+
+@Service
+@Slf4j
+@RequiredArgsConstructor
 public class KafkaProducer {
+    private static final String TOPIC = "orders";
+
+    private final KafkaTemplate<String, String> kafkaTemplate;
+
+    @Scheduled
+    public void sendMessage() {
+        String message = "Next order reveiver at " + Calendar.getInstance().getTime();
+        log.info("Kafka Message sent : {}", message);
+        kafkaTemplate.send(TOPIC, message);
+    }
 }
